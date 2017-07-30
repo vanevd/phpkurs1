641c3c10f04304195267ecd9dbe548cdcfa3ffa5
Dimitar Vanev <radibase@gmail.com>
2017-07-30 13:15:08 +0300

    hotel

--- hotel/js/app.js
+++ hotel/js/app.js
@@ -8,7 +8,38 @@ $(function() {
     $('.more_options_link').click(function() {
         $('.more_options').toggle();
         return false;
-    });    
+    });  
 
     $('.more_options').hide();
+
+    loadHotels();
 });       
+
+
+function loadHotels() {
+    $.ajax({
+        url: "loadHotels.php",
+        type: "GET",
+        data: {
+        },
+        success: function(result){
+            hotels=result;
+            console.log(hotels);
+            $('#hotels').html('');
+            for (key in hotels) {
+                html = $('#hotel-template').html();
+                html = html.replace('{hotel-id}',hotels[key].id);
+                $('#hotels').append(html);
+                $('[data-hotel-id='+hotels[key].id+'] [data="hotel-name"]').html(hotels[key].name);
+                $('[data-hotel-id='+hotels[key].id+'] [data="hotel-pic"]').attr('src', hotels[key].pic);
+                $('[data-hotel-id='+hotels[key].id+'] [data="city"]').html(hotels[key].city);
+                $('[data-hotel-id='+hotels[key].id+'] [data="address"]').html(hotels[key].address);
+                $('[data-hotel-id='+hotels[key].id+'] [data="tel"]').html(hotels[key].tel);
+                $('[data-hotel-id='+hotels[key].id+'] [data="email"]').html(hotels[key].email);
+            }    
+        },
+        error: function(result){
+        }
+    });    
+}
+
---
+++ hotel/loadHotels.php
@@ -0,0 +1,62 @@
+<?php
+
+$hotels = [
+    [
+        "id" => 1,
+        "name" => "Grand Hotel",
+        "pic" => "image/hotel.jpg",
+        "city" => "Pamporovo",
+        "adress" => "15 Bulgaria Str",
+        "tel" => "+359 878 150 111",
+        "email" => "hotel@abv.bg",
+    ],
+    [
+        "id" => 2,
+        "name" => "Novotel",
+        "pic" => "image/hotel1.jpg",
+        "city" => "Pamporovo",
+        "adress" => "15 Bulgaria Str",
+        "tel" => "+359 878 150 111",
+        "email" => "hotel@abv.bg",
+    ],
+    [
+        "id" => 3,
+        "name" => "Borovetz",
+        "pic" => "image/hotel2.jpg",
+        "city" => "Pamporovo",
+        "adress" => "15 Bulgaria Str",
+        "tel" => "+359 878 150 111",
+        "email" => "hotel@abv.bg",
+    ],
+    [
+        "id" => 4,
+        "name" => "Rila",
+        "pic" => "image/hotel3.jpg",
+        "city" => "Pamporovo",
+        "adress" => "15 Bulgaria Str",
+        "tel" => "+359 878 150 111",
+        "email" => "hotel@abv.bg",
+    ],
+    [
+        "id" => 5,
+        "name" => "Rila",
+        "pic" => "image/hotel3.jpg",
+        "city" => "Pamporovo",
+        "adress" => "15 Bulgaria Str",
+        "tel" => "+359 878 150 111",
+        "email" => "hotel@abv.bg",
+    ],
+    [
+        "id" => 6,
+        "name" => "Rila",
+        "pic" => "image/hotel3.jpg",
+        "city" => "Pamporovo",
+        "adress" => "15 Bulgaria Str",
+        "tel" => "+359 878 150 111",
+        "email" => "hotel@abv.bg",
+    ]
+];
+
+header('Content-Type: application/json');
+echo json_encode($hotels);
+
