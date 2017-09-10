9a5a98dffef790b940b70d719d423679943d242a
Dimitar Vanev <radibase@gmail.com>
2017-09-10 13:24:47 +0300

    queue

--- test3.js
+++ test3.js
@@ -169,3 +169,168 @@ function compare_clients(a,b) {
     return 0;
 }
 clients = [client4,client5,client6,client7,client8];
+
+function facturiel(n) {
+    var res = 1;
+    for (i=1; i<=n; i++) {
+        res = res * i;
+    }
+    return res;
+}
+
+function facturielr(n) {
+    if (n == 1) {
+        return facturielr(n-1) * n;
+        //return 1;
+    } else {
+        return facturielr(n-1) * n;
+    }    
+}
+
+function Person(name) {
+    this.name = name;
+    this.childs = [];
+    this.next = null;
+    this.prev = null;
+}
+
+Person.prototype.add_child = function(p) {
+    this.childs.push(p)
+}
+
+Person.prototype.print_names = function() {
+    var i;
+    console.log(this.name);
+    for (i=0; i < this.childs.length; i++) {
+        this.childs[i].print_names();
+    }
+}
+
+function Queue() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+}
+
+Queue.prototype.push = function(p) {
+    if ((typeof p == "object")&&(p.constructor.name == "Person")) {
+        if (this.last) {
+            this.last.next = p;
+            p.prev = this.last;
+            this.last = p;
+        } else {
+            this.first = p;
+            this.last = this.first;
+        }  
+        this.length++;
+    } else {
+        console.log('Invalid Person');
+    }    
+}
+
+Queue.prototype.pop = function() {
+    var res = this.first;
+    if (this.first) {
+        if (this.first.next) {
+            this.first = this.first.next;
+            this.first.prev = null;
+        } else {
+            this.first = null;
+            this.last = null;
+        }
+        this.length--;
+    }    
+    return res;
+}
+
+function Stack() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+}
+
+Stack.prototype.push = function(p) {
+    if ((typeof p == "object")&&(p.constructor.name == "Person")) {
+        if (this.last) {
+            this.last.next = p;
+            p.prev = this.last;
+            this.last = p;
+        } else {
+            this.first = p;
+            this.last = this.first;
+        }  
+        this.length++;
+    } else {
+        console.log('Invalid Person');
+    }    
+}
+
+Stack.prototype.pop = function() {
+    var res = this.last;
+    if (this.last) {
+        if (this.last.prev) {
+            this.last = this.last.prev;
+            this.last.next = null;
+        } else {
+            this.first = null;
+            this.last = null;
+        }
+        this.length--;
+    }    
+    return res;
+}
+
+tp = new Person("Ivan");
+tp.add_child(new Person("Dimitar"));
+tp.add_child(new Person("Stojan"));
+tp.childs[0].add_child(new Person("Maria"));
+tp.childs[0].add_child(new Person("Petya"));
+tp.childs[0].add_child(new Person("Mariana"));
+tp.childs[1].add_child(new Person("Mara"));
+tp.childs[1].add_child(new Person("Pena"));
+tp.childs[1].childs[0].add_child(new Person("Dragan"));
+tp.childs[1].childs[0].add_child(new Person("Petkan"));
+tp.childs[1].childs[0].childs[0].add_child(new Person("Pencho"));
+tp.childs[1].childs[0].childs[1].add_child(new Person("Stojanka"));
+
+first = new Person("Ivan");
+last = first;
+
+i = new Person("Dimitar");
+last.next = i;
+i.prev = last;
+last = i;
+
+i = new Person("Stoyan");
+last.next = i;
+i.prev = last;
+last = i;
+
+i = new Person("Maria");
+last.next = i;
+i.prev = last;
+last = i;
+
+q = new Queue();
+s = new Stack();
+q.push(new Person("Ivan"));
+q.push(new Person("Dimitar"));
+q.push(new Person("Petar"));
+q.push(new Person("Todor"));
+q.push(new Person("Blagovest"));
+q.push(new Person("Irina"));
+q.push(new Person("Petya"));
+q.push(new Person("Maria"));
+
+/*
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+*/
