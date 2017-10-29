33cc30da9ae19bc24cc3266c509d7aee48f052a8
Dimitar Vanev <radibase@gmail.com>
2017-10-29 12:13:33 +0200

    fields

--- js/testjs1.js
+++ js/testjs1.js
@@ -1,10 +1,3 @@
-
-    var c=8;
-    function sum1(a,b){
-        var c=a+b;
-        console.log(c);
-    }
-
     function sum(){
         var s = 0;
         var n = 1;
@@ -69,17 +62,58 @@
 
     }
 
+    function add_field() {
+        var s = '';
+        var n = $('[name=field]').length + 1;
+        s += '<div class="field_row" data-num="' + n +'">';
+        s += '<div  class="label">Field ' + n + '</div><input type="text" name="field" value="">';
+        s += ' <input type="button" value="x" class="delete_row">';
+        s += '</div>';
+        console.log(s);
+        $('#fields').append(s);
+        $(".delete_row:last").click(function () {
+            $(this).closest('.field_row').remove();
+            renumerate();
+        });
+    }
+
+    function delete_fields() {
+        $('#fields').html('');
+    }
+ 
+    function clear_fields() {
+        $('[name=field]').val('');
+    }
+
     function hide(){
         $("#sum_form").hide();
 
     }
+    
     function show(){
         $("#sum_form").show();
     }
 
+    function renumerate() {
+        var n = 1;
+        $('[name=field]').each(function() {
+            num = $(this).closest('.field_row').attr('data-num');
+            $('[data-num='+num+'] .label').html('Field '+n);
+            $('[data-num='+num+']').attr('data-num',n);
+            n++;
+        });
+    }
+
     $(function() {
+        $("#add_field").click(add_field);
+        $("#delete_fields").click(delete_fields);
+        $("#clear_fields").click(clear_fields);
         $("#sum_button").click(sum);
         $("#sum_ajax_button").click(sum_ajax);
         $("#show_button").click(show);
         $("#hide_button").click(hide);
+        $(".delete_row").click(function () {
+            $(this).closest('.field_row').remove();
+            renumerate();
+        });
     });
