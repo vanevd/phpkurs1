af3d999632639132f8257d4e0fac7181a5173ada
Dimitar Vanev <radibase@gmail.com>
2018-01-21 13:22:21 +0200

    mysql

--- myapp/Providers/JsonProvider.php
+++ myapp/Providers/JsonProvider.php
@@ -9,6 +9,13 @@ class JsonProvider extends BaseProvider implements ModelInterface
     public function list()
     {
         $json = file_get_contents("json/" . $this->table . ".json");
+        $items = json_decode($json, true);
+        foreach ($items as $item) {
+            foreach ($this->fields as $field) {
+                echo "$field" . ": " . $item[$field] . "\n";
+            }
+            echo "\n";
+        }
     }
 
 }
